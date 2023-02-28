d4c0bdaa440afbd825225e53a32dc521bd28013c
kadenyi <treykadenyi@gmail.com>
2023-02-28 14:03:24 +0300

    create corsoptions

---
+++ config/corsOptions.js
@@ -0,0 +1,18 @@
+const whiteList = [
+  "https://localhost:3000",
+  "https://www.google.com",
+  "https://localhost:4000",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
