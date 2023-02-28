7b0b13e661fc617ca609b443855cff3ff92dbcfe
kadenyi <treykadenyi@gmail.com>
2023-02-28 21:26:50 +0300

    Add: secure - true samesite - none

--- controllers/authController.js
+++ controllers/authController.js
@@ -27,23 +27,33 @@ const handleLogin = async (req, res) => {
   if (isMatch) {
     // create JWTs
     const accessToken = jwt.sign(
-      { "username": foundUser.username },
+      { username: foundUser.username },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "60s" }
     );
     // refresh token
     const refreshToken = jwt.sign(
-      { "username": foundUser.username },
+      { username: foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
       { expiresIn: "1d" }
     );
-    
-    const otherUsers = usersDB.users.filter((person) => person.username !== foundUser.username);
+
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
     const currentUser = { ...foundUser, refreshToken };
     usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(path.join(__dirname, "..", "model", "users.json"), JSON.stringify(usersDB.users));
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
 
-    res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000})
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
     res.json({ accessToken });
   } else {
     res.sendStatus(401); // unauthorized
