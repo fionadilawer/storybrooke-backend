9d0aac3ffd3c155ce254a36b92474745e44fdb34
kadenyi <treykadenyi@gmail.com>
2023-03-02 15:32:20 +0300

    Create collection in mongodb

---
+++ model/Employee.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EmployeeSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Employee", EmployeeSchema);
