492b9133c5b55f133c5dec6b0572314ebf34fc91
kadenyi <treykadenyi@gmail.com>
2023-03-03 13:33:04 +0300

    Add update route

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,7 @@ const verifyRoles = require("../../middleware/verifyRoles");
 router
   .route("/")
   .get(usersController.getAllUsers)
+  .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), usersController.updateUser)
   .delete(verifyRoles(ROLES_LIST.Admin), usersController.deleteUser);
 
 router.route("/:id").get(usersController.getUser);
