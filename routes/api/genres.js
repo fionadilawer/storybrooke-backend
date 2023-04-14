d32a121c5989bfbb434f01002ab4112f64e536b6
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-14 15:22:18 +0300

    Add various CRUD operators for the genres route

--- routes/api/genres.js
+++ routes/api/genres.js
@@ -7,8 +7,11 @@ const verifyRoles = require("../../middleware/verifyRoles");
 router
   .route("/")
   .get(genresController.getAllGenres)
-  // .post(genresController.createNewGenre);
-  .post(verifyRoles(ROLES_LIST.Admin), genresController.setAllGenres);
+  .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), genresController.createNewGenre)
+  // .post(verifyRoles(ROLES_LIST.Admin), genresController.setAllGenres)
+  .delete(verifyRoles(ROLES_LIST.Admin), genresController.deleteAllGenres)
+  .delete(verifyRoles(ROLES_LIST.Admin), genresController.deleteGenre);
+
 
 router.route("/:id").get(genresController.getGenre);
 
