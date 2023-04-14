b310275c0db2fbfa6eb6b29704d65af5f142b082
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-14 20:44:58 +0300

    Comment out a route

--- routes/api/genres.js
+++ routes/api/genres.js
@@ -7,11 +7,10 @@ const verifyRoles = require("../../middleware/verifyRoles");
 router
   .route("/")
   .get(genresController.getAllGenres)
-  .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), genresController.createNewGenre)
-  // .post(verifyRoles(ROLES_LIST.Admin), genresController.setAllGenres)
+  // .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), genresController.createNewGenre)
+  .post(verifyRoles(ROLES_LIST.Admin), genresController.setAllGenres)
   .delete(verifyRoles(ROLES_LIST.Admin), genresController.deleteAllGenres)
   .delete(verifyRoles(ROLES_LIST.Admin), genresController.deleteGenre);
-
 
 router.route("/:id").get(genresController.getGenre);
 
