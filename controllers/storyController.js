7907995f1806c89bf4c0e7cd3f777b46c6bcbe21
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-16 00:50:43 +0300

    Update controller to: Prevent plagiarism when creating new stories Edit the update story function Check for unique body content instead of title when fetching all stories

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -22,13 +22,28 @@ const createStory = async (req, res) => {
     }
   }
 
+  //  check if story exists in any genre
+  const result = await Genre.findOne({
+    stories: { $elemMatch: { body: story.body } },
+  }).exec();
+
+  if (result) {
+    return res.status(400).json({
+      message: `The story you're trying to add already exists in genre ${result.genre}. Please come up with a new story. If you want to add this story to another genre, please use the update story route.`,
+    });
+  }
+
   //   find the genres and add the story to them
   for (let i = 0; i < req.body.genres.length; i++) {
     // find genre in db
     const genre = await Genre.findOne({ genre: story.genres[i] }).exec();
 
     // check if story exists in the genre
-    if (genre.stories.find((story) => story.title === req.body.title)) continue;
+    if (genre.stories.find((story) => story.body === req.body.body)) {
+      return res.status(400).json({
+        message: `The story you're trying to add already exists in this genre. Please come up with a new story.`,
+      });
+    }
 
     // push story to genre
     genre.stories.push(story);
@@ -116,39 +131,55 @@ const getStoryAllGenres = async (req, res) => {
   res.status(200).json(result);
 };
 
-// update a story in a genre
+// update a story globally
 const updateStory = async (req, res) => {
-  // check if genre exists
-  const genre = await Genre.findOne({ genre: req.params.genre }).exec();
+  // check if params are empty
+  if (!req.params.title)
+    return res.status(400).json({ message: "Story title is required." });
 
-  if (!genre) {
-    return res
-      .status(404)
-      .json({ message: `Genre ${req.params.genre} not found.` });
-  }
+  // check if story exists in any genre in db
+  const story = await Genre.findOne({
+    stories: { $elemMatch: { title: req.params.title } },
+  }).exec();
 
-  // check if story exists
-  const story = genre.stories.find((story) => story.title === req.body.title);
-
+  // if story doesn't exist
   if (!story) {
-    return res
-      .status(404)
-      .json({ message: `Story ${req.body.title} not found.` });
+    return res.status(404).json({
+      message: `Story ${req.params.title} not found in the database.`,
+    });
   }
 
-  // update story
-  story.title = req.body.title;
-  story.author = req.body.author;
-  story.body = req.body.body;
-  story.date = new Date();
+  // remove story from the db
+  const result = await Genre.updateMany(
+    { stories: { $elemMatch: { title: req.params.title } } },
+    { $pull: { stories: { title: req.params.title } } }
+  ).exec();
 
-  // save genre
-  try {
-    const result = await genre.save();
-    res.status(200).json(result);
-  } catch (err) {
-    console.error(err);
-  }
+  // check if body is empty
+  if (!req.body.title || !req.body.author || !req.body.body || !req.body.genres)
+    return res.status(400).json({
+      message: `All fields are required. Please enter the title, author, body, and genres.`,
+    });
+
+  // create the new story
+  const newStory = {
+    title: req.body.title,
+    author: req.body.author,
+    body: req.body.body,
+    genres: req.body.genres,
+    date: new Date(),
+  };
+
+  // add story to all genres
+  const result2 = await Genre.updateMany(
+    { genre: { $in: req.body.genres } },
+    { $push: { stories: newStory } }
+  ).exec();
+
+  res.status(200).json({
+    // display new story
+    message: `Story ${req.params.title} successfully updated in genres ${req.body.genres}.`,
+  });
 };
 
 // get all stories in all genres
@@ -169,12 +200,11 @@ const getAllStoriesGlobal = async (req, res) => {
     return res.status(404).json({ message: `No stories found.` });
   }
 
-  // return unique story titles only
+  // return stories only if the content of the body is unique
   stories = stories.filter(
     (story, index, self) =>
-      index === self.findIndex((t) => t.title === story.title)
+      index === self.findIndex((b) => b.body === story.body)
   );
-
   res.status(200).json(stories);
 };
 
