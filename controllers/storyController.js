480dc16374720052fd13acd6525f99b343627ed6
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-17 00:15:01 +0300

    Finesse CRUD: Alter create operator by: Checking if duplicate genres exist and removing them Checking if similar titles exist within the same genre and preventing addition Adding exception messages to explain why some genres are being removed

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -15,21 +15,21 @@ const createStory = async (req, res) => {
 
   for (let i = 0; i < story.genres.length; i++) {
     const genre = await Genre.findOne({ genre: story.genres[i] }).exec();
-    if (!genre) {
+    if (!genre || story.genres.indexOf(story.genres[i]) !== i) {
       story.genres.splice(i, 1);
       i--;
       continue;
     }
   }
 
-  //  check if story exists in any genre
+  //  check if story content/body exists in any genre
   const result = await Genre.findOne({
     stories: { $elemMatch: { body: story.body } },
   }).exec();
 
   if (result) {
     return res.status(400).json({
-      message: `The story you're trying to add already exists in genre ${result.genre}. Please come up with a new story. If you want to add this story to another genre, please use the update story route.`,
+      message: `The story you're trying to add already exists in at least one genre in the database (i.e. ${result.genre}). Please come up with a new story. If you want to add this story to another genre, please use the update story route.`,
     });
   }
 
@@ -39,10 +39,18 @@ const createStory = async (req, res) => {
     const genre = await Genre.findOne({ genre: story.genres[i] }).exec();
 
     // check if story exists in the genre
-    if (genre.stories.find((story) => story.body === req.body.body)) {
-      return res.status(400).json({
-        message: `The story you're trying to add already exists in this genre. Please come up with a new story.`,
-      });
+    // if (genre.stories.find((story) => story.body === req.body.body)) {
+    //   return res.status(400).json({
+    //     message: `The story you're trying to add already exists in this genre. Please come up with a new story.`,
+    //   });
+    // }
+
+    // check if story title exists in the genre
+    if (genre.stories.find((story) => story.title === req.body.title)) {
+      // remove the genre from the story's genres array
+      story.genres.splice(i, 1);
+      i--;
+      continue;
     }
 
     // push story to genre
@@ -57,8 +65,15 @@ const createStory = async (req, res) => {
     }
   }
 
+  // if by this point no genres are valid, return message
+  if (story.genres.length === 0) {
+    return res.status(400).json({
+      message: `It appears that none of the genres you specified were valid options. Please go through the following exceptions to understand what went wrong Exceptions: 1. A genre is not allowed to have multiple similar titles for a story as this creates confusion for the user. Please change the title of the story and try again. 2. If the story's content already exists in the database, the story will not be added to any genre. We encourage originality and creativity. Therefore, each story must be unique. While titles can be shared across genres, the content must be unique. 3. If the genre you specified does not exist, the story will not be added to that genre. Please check the spelling of the genre and try again. Thank you for your understanding.`,
+    });
+  }
+
   res.status(201).json({
-    message: `Story ${story.title} created in genres ${story.genres}.`,
+    message: `Story ${story.title} created in genres ${story.genres}. Exceptions: 1. If the story's title already exists in a specified genre or a genre simply doesn't exist, the story will not be added to the specified genres. 2. Moreover, if the story's content already exists in the database, the story will not be added to any genre. We encourage originality and creativity. Therefore, each story must be unique. While titles can be shared across genres, the content must be unique. Also, it creates less confusion for the user if a specific genre does not have multiple stories with the same title. Thank you for your understanding.`,
   });
 };
 
@@ -211,12 +226,22 @@ const getAllStoriesGlobal = async (req, res) => {
 // delete a story in all genres
 const deleteStory = async (req, res) => {
   // check if empty
-  if (!req.body.title)
-    return res.status(400).json({ message: "Title is required." });
+  // if (!req.body.title || !req?.body?.id)
+  //   return res.status(400).json({ message: "Title and id are required." });
 
-  // check if story exists in any genre
+  // // check if story exists in any genre
+  // const story = await Genre.findOne({
+  //   stories: { $elemMatch: { title: req.body.title } },
+  // }).exec();
+
+  // check if no id and title provided
+  if (!req.body.title || !req.body.id) {
+    return res.status(400).json({ message: "Title and id are required." });
+  }
+
+  // check if story exists in any genre by id
   const story = await Genre.findOne({
-    stories: { $elemMatch: { title: req.body.title } },
+    stories: { $elemMatch: { _id: req.body.id } },
   }).exec();
 
   if (!story) {
@@ -226,12 +251,33 @@ const deleteStory = async (req, res) => {
   }
 
   // remove story from all genres
-  const result = await Genre.updateMany(
-    {},
-    { $pull: { stories: { title: req.body.title } } }
-  ).exec();
+  // const result = await Genre.updateMany(
+  //   {},
+  //   { $pull: { stories: { title: req.body.title } } }
+  // ).exec();
 
-  res.status(200).json(result);
+  // res.status(200).json(result);
+
+  // check db for story with id and title
+  const story2 = await Genre.findOne({
+    stories: { $elemMatch: { _id: req.body.id, title: req.body.title } },
+  }).exec();
+
+  // if id and title belong to the same story in the db then remove it from all genres
+  if (story2) {
+    const result = await Genre.updateMany(
+      {},
+      { $pull: { stories: { title: req.body.title } } }
+    ).exec();
+    res.status(200).json({
+      message: `Story ${req.body.title} successfully deleted from all genres.`,
+    });
+  }
+
+  // if id and title don't belong to the same story in the db then return error
+  else {
+    res.status(404).json({ message: `Story ${req.body.title} not found.` });
+  }
 };
 
 // delete a story in a specific genre
