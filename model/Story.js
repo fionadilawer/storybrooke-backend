8f78afbaffefec0e8b1b3a01b47d5319db0d6ace
Tracey Kadenyi <treykadenyi@gmail.com>
2023-04-17 17:47:39 +0300

    Remove id from the schema

--- model/Story.js
+++ model/Story.js
@@ -2,12 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const StorySchema = new Schema({
-  // create the same id for one story
-  _id: {
-    type: String,
-    required: true,
-    default: new mongoose.Types.ObjectId().toHexString(),
-  },
   title: {
     type: String,
     required: true,
