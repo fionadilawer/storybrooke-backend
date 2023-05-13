f4d37342a28c18ae1646a794d17c7c296f5d6cdf
Tracey Kadenyi <treykadenyi@gmail.com>
2023-05-13 21:48:28 +0300

    Add first and last name to user model

--- model/User.js
+++ model/User.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
