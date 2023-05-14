7d3b0fd88591875440e6f6b87ccde09568234b55
Tracey Kadenyi <treykadenyi@gmail.com>
2023-05-14 15:21:49 +0300

    Add the post route

---
+++ routes/api/profiles.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const ROLES_LIST = require("../../config/rolesList");
+const profilesController = require("../../controllers/profilesController");
+const verifyRoles = require("../../middleware/verifyRoles");
+
+// global routes
+router.route("/").post(profilesController.createProfile);
+
+module.exports = router;
