857002c7b4264d7bba8ef88c79497ca56d6c5f2b
Tracey Kadenyi <treykadenyi@gmail.com>
2023-05-15 19:10:29 +0300

    Add get profile route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -5,6 +5,8 @@ const profilesController = require("../../controllers/profilesController");
 const verifyRoles = require("../../middleware/verifyRoles");
 
 // global routes
-router.route("/").post(profilesController.createProfile);
+// router.route("/").post(profilesController.createProfile);
+router.route("/")
+.get(profilesController.getProfile)
 
 module.exports = router;
