b3ddcc9f566b9ac1f55c0d334b4b590892d8b19e
Tracey Kadenyi <treykadenyi@gmail.com>
2023-05-19 22:24:38 +0300

    Reshuffle and delete comments

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -3,72 +3,6 @@ const User = require("../model/User");
 const Genre = require("../model/Genre");
 const Story = require("../model/Story");
 
-// const createProfile = async (req, res) => {
-//   const { firstname, lastname, username, bio, profilePicture } = req.body;
-//   const newFirstName = firstname
-//     ? firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase()
-//     : null;
-//   const newLastName = lastname
-//     ? lastname.charAt(0).toUpperCase() + lastname.slice(1).toLowerCase()
-//     : null;
-//   const newUserName = username
-//     ? username.charAt(0).toUpperCase() + username.slice(1).toLowerCase()
-//     : null;
-
-//   if (!firstname || !lastname || !username) {
-//     return res
-//       .status(400)
-//       .json({ message: "First name, last name, and username are required." });
-//   }
-
-//   // check if user exists in the database
-//   const user = await User.findOne({ username: newUserName }).exec();
-
-//   if (!user) {
-//     return res
-//       .status(404)
-//       .json({ message: `No user with username ${req.body.username} found.` });
-//   } else if (user) {
-//     // check if first name, last name, and username match
-//     if (
-//       user.firstname !== newFirstName ||
-//       user.lastname !== newLastName ||
-//       user.username !== newUserName
-//     ) {
-//       return res.status(400).json({
-//         message: `First name, last name, and username do not match.`,
-//       });
-//     }
-//   }
-
-//   // check if profile already exists
-
-//   const profileExists = await Profile.findOne({ username: newUserName }).exec();
-
-//   if (profileExists) {
-//     return res.status(400).json({
-//       message: `Profile with username ${req.body.username} already exists. If you wish to update your profile, please use the update profile endpoint.`,
-//     });
-//   }
-
-//   // create profile
-//   const profile = new Profile({
-//     firstname: newFirstName,
-//     lastname: newLastName,
-//     username: newUserName,
-//     bio: bio,
-//     profilePicture: profilePicture,
-//     dateJoined: new Date(),
-//   });
-
-//   try {
-//     const result = await profile.save();
-//     res.status(201).json(result);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
 // GET USER PROFILE
 const getProfile = async (req, res) => {
   // check if no params
@@ -137,9 +71,6 @@ const updateProfile = async (req, res) => {
     });
   }
 
-  // delete old profile
-  await Profile.findOneAndDelete({ username: newUserName }).exec();
-
   // create new profile
   const profile = new Profile({
     firstname: profileExists.firstname,
@@ -168,7 +99,10 @@ const updateProfile = async (req, res) => {
     return res.status(400).json({
       message: `Username ${profile.updatedUserName} is already taken.`,
     });
-  };
+  }
+
+  // delete old profile
+  await Profile.findOneAndDelete({ username: newUserName }).exec();
 
   try {
     const result = await profile.save();
