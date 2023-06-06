7cfd689e11dd73cf907503d17737d3a6d87df425
Tracey Kadenyi <treykadenyi@gmail.com>
2023-06-06 21:27:44 +0300

    Modify routes

--- routes/api/stories.js
+++ routes/api/stories.js
@@ -4,39 +4,50 @@ const ROLES_LIST = require("../../config/rolesList");
 const storiesController = require("../../controllers/storiesController");
 const verifyRoles = require("../../middleware/verifyRoles");
 
-// global routes
+// GLOBAL ROUTES
 router
   .route("/")
   // .get(storiesController.getAllStories)
+
   .post(storiesController.createStory)
+
   .delete(
     verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
     storiesController.deleteStory
   )
+
   .get(storiesController.getAllStoriesGlobal);
 
-// specific genre
+// SPECIFIC ROUTES
 router
   .route("/:id")
-  .put(
-    verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
-    storiesController.updateStory
-  );
-router.route("/title/:title").get(storiesController.getStoryAllGenres);
 
+  // update story in the database by ID
+  .put(
+    // verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+    storiesController.updateStory
+  )
+
+  // get story from the database by ID
+  .get(storiesController.getStoryById);
+
+// get story from the database by title
+router.route("/title/:title").get(storiesController.getStoryGlobal);
+
+// count all the stories in the database
 router.route("/count").get(storiesController.countStoriesGlobal);
 
+// get all stories by author
 router.route("/author/:author").get(storiesController.getStoriesByAuthor);
 
-router
-  .route("/:genre")
-  .get(storiesController.getAllStories)
+// get all stories in a genre
+router.route("/find/:genre").get(storiesController.getAllStoriesInGenre);
 
-  // .delete(
-  //   verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
-  //   storiesController.deleteStoryGenre
-  // );
+// .delete(
+//   verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),
+//   storiesController.deleteStoryGenre
+// );
 
-router.route("/:genre/find").get(storiesController.getStory);
+// router.route("/:genre/find").get(storiesController.getStory);
 
 module.exports = router;
