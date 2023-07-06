99283bf8fd3613c8178d16c1bad7bd760d74c95c
Tracey Kadenyi <treykadenyi@gmail.com>
2023-07-06 14:02:50 +0300

    Add acceptable url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://www.google.com",
   "http://localhost:4000",
+  "https://imagine-tellmeastory.vercel.app",
 ];
 
 module.exports = allowedOrigins;
