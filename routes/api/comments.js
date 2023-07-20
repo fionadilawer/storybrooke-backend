ed349221300cb286246a6ee51be28519cfe66e23
Tracey Kadenyi <treykadenyi@gmail.com>
2023-07-20 22:46:54 +0300

    Get and delete comment routes

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const commentsController = require("../../controllers/commentsController");
 
 // Create a new comment
-router.route("/:id").post(commentsController.createComment);
+router
+  .route("/:id")
+
+  .post(commentsController.createComment)
+
+  // Get a comment by id
+  .get(commentsController.getComments)
+
+  // Delete a comment by id
+  .delete(commentsController.deleteComment);
 
 module.exports = router;
