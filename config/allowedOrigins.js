0b0b65b056b9eb53c7e7d40312e48a15de83af0d
Tracey Kadenyi <treykadenyi@gmail.com>
2023-08-06 12:41:45 +0300

    Add origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
   // "https://imagine-tellmeastory-git-dev-trace-kadenyi.vercel.app/",
   "https://imagine-tellmeastory.vercel.app",
-  // "http://localhost:3000",
+  "http://localhost:3000",
   "http://localhost:4000",
 ];
 
